94727e35bd683b65ed96630207e9d539a72bd0c4
An <antr.2201@gmail.com>
2020-03-20 04:37:45 +0100

    notifications screen wip

--- screens/LockScreen/Notifications.js
+++ screens/LockScreen/Notifications.js
@@ -4,17 +4,44 @@ import {
   StyleSheet,
   View,
   Text,
+  ScrollView
 } from 'react-native';
 import styled from 'styled-components';
 
-const Button = styled.Text`
-  position: absolute;
-  bottom: 84px;
-  font-size: 14px;
+const DateTime = styled.View``;
+
+const Time = styled.Text`
   font-weight: bold;
-  background-color: #c4c4c4;
-  padding: 12px;
-  padding-right: 48px;
+  font-size: 96px;
+  line-height: 96px;
+`
+
+const Date = styled.Text`
+  font-size: 11px;
+`;
+
+const NotificationsList = styled.ScrollView`
+  width: 80%;
+  max-height: 56%;
+  margin: 24px 0;
+`;
+
+const Swiper = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SwiperText = styled.Text`
+  font-size: 11px;
+  letter-spacing: 0.8px;
+`;
+
+const SwiperNotch = styled.View`
+  margin-top: 5px;
+  width: 100px;
+  height: 5px;
+  background-color: #565656;
 `;
 
 const NotificationsScreen = () => {
@@ -22,12 +49,18 @@ const NotificationsScreen = () => {
     <>
       <SafeAreaView>
         <View style={styles.body}>
-          <View style={styles.logo}></View>
-          <Text style={styles.title}>Notifications</Text>
-          <Text style={styles.content}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non arcu lobortis, lobortis ipsum et, aliquet leo. Vestibulum tincidunt turpis pretium neque vulputate, nec imperdiet velit ultricies. In ac quam ex. Ut leo libero, ultrices volutpat velit ut, vehicula bibendum nibh. Vestibulum lacus lectus, tincidunt vitae mauris quis, semper condimentum ante.
-          </Text>
-          <Button>Suivant</Button>
+          <DateTime>
+            <Time>13</Time>
+            <Time>45</Time>
+            <Date>Mardi 17 Mars 2020</Date>
+          </DateTime>
+          <NotificationsList>
+            <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non arcu lobortis, lobortis ipsum et, aliquet leo. Vestibulum tincidunt turpis pretium neque vulputate, nec imperdiet velit ultricies. In ac quam ex. Ut leo libero, ultrices volutpat velit ut, vehicula bibendum nibh. Vestibulum lacus lectus, tincidunt vitae mauris quis, semper condimentum ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non arcu lobortis, lobortis ipsum et, aliquet leo. Vestibulum tincidunt turpis pretium neque vulputate, nec imperdiet velit ultricies. In ac quam ex. Ut leo libero, ultrices volutpat velit ut, vehicula bibendum nibh. Vestibulum lacus lectus, tincidunt vitae mauris quis, semper condimentum ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non arcu lobortis, lobortis ipsum et, aliquet leo. Vestibulum tincidunt turpis pretium neque vulputate, nec imperdiet velit ultricies. In ac quam ex. Ut leo libero, ultrices volutpat velit ut, vehicula bibendum nibh. Vestibulum lacus lectus, tincidunt vitae mauris quis, semper condimentum ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non arcu lobortis, lobortis ipsum et, aliquet leo. Vestibulum tincidunt turpis pretium neque vulputate, nec imperdiet velit ultricies. In ac quam ex. Ut leo libero, ultrices volutpat velit ut, vehicula bibendum nibh. Vestibulum lacus lectus, tincidunt vitae mauris quis, semper condimentum ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non arcu lobortis, lobortis ipsum et, aliquet leo. Vestibulum tincidunt turpis pretium neque vulputate, nec imperdiet velit ultricies. In ac quam ex. Ut leo libero, ultrices volutpat velit ut, vehicula bibendum nibh. Vestibulum lacus lectus, tincidunt vitae mauris quis, semper condimentum ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non arcu lobortis, lobortis ipsum et, aliquet leo. Vestibulum tincidunt turpis pretium neque vulputate, nec imperdiet velit ultricies. In ac quam ex. Ut leo libero, ultrices volutpat velit ut, vehicula bibendum nibh. Vestibulum lacus lectus, tincidunt vitae mauris quis, semper condimentum ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non arcu lobortis, lobortis ipsum et, aliquet leo. Vestibulum tincidunt turpis pretium neque vulputate, nec imperdiet velit ultricies. In ac quam ex. Ut leo libero, ultrices volutpat velit ut, vehicula bibendum nibh. Vestibulum lacus lectus, tincidunt vitae mauris quis, semper condimentum ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non arcu lobortis, lobortis ipsum et, aliquet leo. Vestibulum tincidunt turpis pretium neque vulputate, nec imperdiet velit ultricies. In ac quam ex. Ut leo libero, ultrices volutpat velit ut, vehicula bibendum nibh. Vestibulum lacus lectus, tincidunt vitae mauris quis, semper condimentum ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non arcu lobortis, lobortis ipsum et, aliquet leo. Vestibulum tincidunt turpis pretium neque vulputate, nec imperdiet velit ultricies. In ac quam ex. Ut leo libero, ultrices volutpat velit ut, vehicula bibendum nibh. Vestibulum lacus lectus, tincidunt vitae mauris quis, semper condimentum ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non arcu lobortis, lobortis ipsum et, aliquet leo. Vestibulum tincidunt turpis pretium neque vulputate, nec imperdiet velit ultricies. In ac quam ex. Ut leo libero, ultrices volutpat velit ut, vehicula bibendum nibh. Vestibulum lacus lectus, tincidunt vitae mauris quis, semper condimentum ante.</Text>
+          </NotificationsList>
+          <Swiper>
+            <SwiperText>Swipe to unlock</SwiperText>
+            <SwiperNotch />
+          </Swiper>
         </View>
       </SafeAreaView>
     </>
@@ -36,30 +69,11 @@ const NotificationsScreen = () => {
 
 const styles = StyleSheet.create({
   body: {
-    backgroundColor: '#565656',
+    backgroundColor: '#c4c4c4',
     width: '100%',
     height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  logo: {
-    backgroundColor: '#c4c4c4',
-    width: 58,
-    height: 58,
-    position: 'absolute',
-    top: 84,
-  },
-  title: {
-    color: '#e8e8e8',
-    marginBottom: 12,
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  content: {
-    color: '#e8e8e8',
-    fontSize: 12,
-    textAlign: 'center',
-    padding: 12,
   },
 });
 

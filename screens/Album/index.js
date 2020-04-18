ed6177ad4fd83ef15d34b36954290880c2f90834
An <antr.2201@gmail.com>
2020-04-18 17:40:55 +0200

    Album app: add Lock overlay

--- screens/Album/index.js
+++ screens/Album/index.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, View, Dimensions } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, Dimensions } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import Modal from 'react-native-modal';
 import styled from 'styled-components';
 
 import NavigationBar from '../../sharedUI/NavigationBar';
 import PhotoThumbnail from '../../sharedUI/PhotoThumbnail';
+import Icon from '../../sharedUI/Icon';
 
 const PhotoGrid = styled.ScrollView`
 	width: 100%;
@@ -12,13 +15,28 @@ const PhotoGrid = styled.ScrollView`
 `;
 
 const LockOverlay = styled.View`
-	background-color: rgba(0, 0, 0, 0.7);
-	width: 100%;
-	height: 100%;
-	opacity: 0.7;
-	position: absolute;
-	top: 0;
-	left: 0;
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+`;
+
+const IconWrapper = styled.View`
+	margin-bottom: 32px;
+`;
+
+const Title = styled.Text`
+	margin-bottom: 12px;
+	font-size: 14px;
+	font-weight: bold;
+	color: #fff;
+`;
+
+const Input = styled.TextInput`
+	width: 64%;
+	background-color: #e8e8e8;
+	font-size: 12px;
+	text-align: center;
+	padding: 8px;
 `;
 
 const AlbumScreen = ({ navigation }) => {
@@ -26,7 +44,10 @@ const AlbumScreen = ({ navigation }) => {
 	const photoSize = deviceW / 3;
 	const photoNb = 32;
 
-	const [isLocked, setIsLocked] = useState(false);
+	const [isLocked, setIsLocked] = useState(true);
+	const [passwordInput, setPasswordInput] = useState('');
+
+	const onSubmitPassword = () => setIsLocked(false);
 
 	return (
 		<>
@@ -42,8 +63,26 @@ const AlbumScreen = ({ navigation }) => {
 							/>
 						))}
 					</PhotoGrid>
+					<Modal isVisible={isLocked} style={styles.modal}>
+						<LockOverlay>
+							<IconWrapper>
+								<Icon type="LOCK" color="#fff" />
+							</IconWrapper>
+							<Title>Entrer le mot de passe</Title>
+							<Input
+								secureTextEntry
+								blurOnSubmit
+								onChangeText={(text) => setPasswordInput(text)}
+								onSubmitEditing={onSubmitPassword}
+								value={passwordInput}
+							/>
+						</LockOverlay>
+						<NavigationBar
+							onPressHome={() => navigation.navigate('HomeScreen')}
+							black
+						/>
+					</Modal>
 				</View>
-				{isLocked && <LockOverlay />}
 				<NavigationBar
 					onPressHome={() => navigation.navigate('HomeScreen')}
 					black
@@ -67,6 +106,10 @@ const styles = StyleSheet.create({
 		flexWrap: 'wrap',
 		justifyContent: 'flex-start',
 		alignItems: 'flex-start',
+	},
+	modal: {
+		flex: 1,
+		margin: 0,
 	},
 });
 

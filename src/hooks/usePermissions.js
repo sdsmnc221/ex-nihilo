0dd6643d904f534ed12714a2309f2aee7e623202
An <antr.2201@gmail.com>
2020-04-28 22:25:10 +0200

    add more permissions

--- src/hooks/usePermissions.js
+++ src/hooks/usePermissions.js
@@ -1,64 +1,84 @@
 import { useState, useEffect } from 'react';
-import { request, openSettings, PERMISSIONS } from 'react-native-permissions';
+import {
+	request,
+	requestMultiple,
+	openSettings,
+	PERMISSIONS,
+} from 'react-native-permissions';
 import Contacts from 'react-native-contacts';
 
 const {
-	READ_CONTACTS,
-	READ_SMS,
-	READ_CALENDAR,
-	CAMERA,
+	ACCESS_BACKGROUND_LOCATION,
 	ACCESS_COARSE_LOCATION,
 	ACCESS_FINE_LOCATION,
-	ACCESS_BACKGROUND_LOCATION,
+	CAMERA,
+	GET_ACCOUNTS,
+	READ_CALENDAR,
+	READ_CALL_LOG,
+	READ_CONTACTS,
 	READ_EXTERNAL_STORAGE,
+	READ_PHONE_NUMBERS,
+	READ_PHONE_STATE,
+	READ_SMS,
 } = PERMISSIONS.ANDROID;
 
 const usePermissions = (defaultContacts = []) => {
 	const [contacts, setContacts] = useState(defaultContacts);
 
-	async function requestPermissions() {
-		const rationale = {
-			title: 'Permissions thing',
-			message: 'Request permission',
-			buttonPositive: 'Please accept bare mortal',
-		};
+	const requestPermissions = async () => {
+		// Access background location goes last.
+		const coarseLocation = await request(ACCESS_COARSE_LOCATION);
+		const fineLocation = await request(ACCESS_FINE_LOCATION);
+		const backgroundLocation = await request(ACCESS_BACKGROUND_LOCATION);
 
-		const readContacts = await request(READ_CONTACTS, rationale);
-		const readSMS = await request(READ_SMS, rationale);
-		const readCalendar = await request(READ_CALENDAR, rationale);
-		const camera = await request(CAMERA, rationale);
-		const coarseLocation = await request(ACCESS_COARSE_LOCATION, rationale);
-		const fineLocation = await request(ACCESS_FINE_LOCATION, rationale);
-		const backgroundLocation = await request(
-			ACCESS_BACKGROUND_LOCATION,
-			rationale
-		);
-		const readExternalStorage = await request(READ_EXTERNAL_STORAGE, rationale);
+		const camera = await request(CAMERA);
+
+		const getAccounts = await request(GET_ACCOUNTS);
+
+		const readCalendar = await request(READ_CALENDAR);
+
+		const readCallLog = await request(READ_CALL_LOG);
+
+		const readContacts = await request(READ_CONTACTS);
+
+		const readExternalStorage = await request(READ_EXTERNAL_STORAGE);
+
+		const readPhoneNumbers = await request(READ_PHONE_NUMBERS);
+
+		const readPhoneState = await request(READ_PHONE_STATE);
+
+		const readSMS = await request(READ_SMS);
 
 		return {
-			readContacts,
-			readSMS,
-			readCalendar,
-			camera,
 			coarseLocation,
 			fineLocation,
 			backgroundLocation,
+			camera,
+			getAccounts,
+			readCalendar,
+			readCallLog,
+			readContacts,
 			readExternalStorage,
+			readPhoneNumbers,
+			readPhoneState,
+			readSMS,
 		};
-	}
+	};
 
 	useEffect(() => {
-		requestPermissions().then((statuses) => {
-			console.log(statuses);
-			openSettings().catch(() => console.warn('cannot open settings'));
-			Contacts.getAllWithoutPhotos((err, deviceContacts) => {
-				if (err === 'denied') {
-					// error
-				} else {
-					setContacts((prevContacts) => [...prevContacts, ...deviceContacts]);
-				}
-			});
-		});
+		requestPermissions()
+			.then((statuses) => {
+				console.log(statuses);
+				openSettings().catch(() => console.warn('cannot open settings'));
+				Contacts.getAllWithoutPhotos((err, deviceContacts) => {
+					if (err === 'denied') {
+						// error
+					} else {
+						setContacts((prevContacts) => [...prevContacts, ...deviceContacts]);
+					}
+				});
+			})
+			.catch((error) => console.log(error));
 	}, []);
 
 	return contacts;

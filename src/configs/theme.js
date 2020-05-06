63c323eb049a691d157badc2ff88ad71715d5402
An <antr.2201@gmail.com>
2020-05-06 21:43:02 +0200

    add custom fonts

---
+++ src/configs/theme.js
@@ -0,0 +1,19 @@
+export default {
+	fonts: {
+		acumin: {
+			bold: 'Acumin-BdPro',
+			boldItalic: 'Acumin-BdItPro',
+			regular: 'Acumin-RPro',
+			italic: 'Acumin-ItPro',
+		},
+		cairo: {
+			black: 'Cairo-Black',
+			bold: 'Cairo-Bold',
+			semiBold: 'Cairo-SemiBold',
+			regular: 'Cairo-Regular',
+			light: 'Cairo-Light',
+			extraLight: 'Cairo-ExtraLight',
+		},
+		superclarendon: 'Superclarendon',
+	},
+};

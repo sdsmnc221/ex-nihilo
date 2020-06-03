c039a25cb04923a6506b7d7990c43d6647379da0
An <antr.2201@gmail.com>
2020-06-03 18:35:29 +0200

    contact detail screen 2

--- src/screens/Contacts/components/ContactInfo.js
+++ src/screens/Contacts/components/ContactInfo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { View, Text } from 'react-native';
 
 import FlexDiv from 'sharedUI/FlexDiv';

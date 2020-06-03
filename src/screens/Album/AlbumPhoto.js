c5510680d23dc0010a91bf351722c8bebba4e2f0
An <antr.2201@gmail.com>
2020-06-03 20:37:36 +0200

    album photo screen

--- src/screens/Album/AlbumPhoto.js
+++ src/screens/Album/AlbumPhoto.js
@@ -1,36 +1,26 @@
 import React from 'react';
-import { StyleSheet, View, Image, Dimensions } from 'react-native';
+import { Image } from 'react-native';
 
 import LayoutWrapper from 'sharedUI/LayoutWrapper';
 
-const AlbumPhotoScreen = ({ route, navigation }) => {
+import { device } from 'utils';
+
+const AlbumPhotoScreen = ({ route }) => {
 	const { uri } = route.params;
-	const { width: deviceW, height: deviceH } = Dimensions.get('window');
+	const { width, height } = device();
 
 	return (
 		<LayoutWrapper screenName={route.name}>
-			<View style={styles.body}>
-				<Image
-					style={{
-						width: deviceW,
-						height: deviceH,
-					}}
-					source={{ uri }}
-					resizeMode="contain"
-				/>
-			</View>
+			<Image
+				style={{
+					width,
+					height,
+				}}
+				source={{ uri }}
+				resizeMode="contain"
+			/>
 		</LayoutWrapper>
 	);
 };
-
-const styles = StyleSheet.create({
-	body: {
-		backgroundColor: '#c4c4c4',
-		width: '100%',
-		height: '100%',
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-});
 
 export default AlbumPhotoScreen;
